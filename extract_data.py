134e3618079efb052e552c245d59b2eb7060d5de
cwhite098 <gd19031@bristol.ac.uk>
2021-10-19 16:22:43 +0100

    added new funcs to extract_data and cleaned up the notebook

--- extract_data.py
+++ extract_data.py
@@ -8,48 +8,67 @@ import os
 def get_datum(json_filename, incident_number):
 
     df_categorised = pd.read_json(json_filename)
-
-    # Get status of incident
-    status = df_categorised['status']
-    status = status[incident_number]
-
-    # Get detailed info
-    accel_df = df_categorised['detail']
-    detail_dict = accel_df[incident_number]
-
-    # Less detailed data
-    gps_df = df_categorised['journey']
-    journey_dict = gps_df[incident_number]
-
-    # Zoomed out data, has long term speed and accelerometer data
-    zoomed_out_df = pd.DataFrame.from_dict(journey_dict)
-
-    # Zoomed in data, use to get the speed in smaller interval
-    zoomed_in_df = pd.DataFrame.from_dict(detail_dict)
-
-    # Extract the higher resolution accelerometer data for the crash
-    forces = zoomed_in_df['forces']
-    zoomed_in_tilts = []
-    for second in forces:
-        force_1 = pd.DataFrame.from_dict(second)
-        zoomed_in_tilts.append(force_1)
-
-    zoomed_in_tilts = pd.concat(zoomed_in_tilts)
-    zoomed_in_tilts = zoomed_in_tilts.rename(columns={'index':'timeoffset'})
-
     linspace_1 = np.linspace(-6, 2.875, 72)
-    zoomed_in_tilts['timeoffset'] = linspace_1
 
     # Changed zoomed_in_df to include t=0 and the other variables
     if str(json_filename) == 'data/uncategorised.json' or json_filename == 'data/categorised.json':
+
+        # Get status of incident
+        status = df_categorised['status']
+        status = status[incident_number]
+        # Get detailed info
+        accel_df = df_categorised['detail']
+        detail_dict = accel_df[incident_number]
+        # Less detailed data
+        gps_df = df_categorised['journey']
+        journey_dict = gps_df[incident_number]
+
+        # Zoomed out data, has long term speed and accelerometer data
+        zoomed_out_df = pd.DataFrame.from_dict(journey_dict)
+        # Zoomed in data, use to get the speed in smaller interval
+        zoomed_in_df = pd.DataFrame.from_dict(detail_dict)
+
+        # Extract the higher resolution accelerometer data for the crash
+        forces = zoomed_in_df['forces']
+        zoomed_in_tilts = []
+        for second in forces:
+            force_1 = pd.DataFrame.from_dict(second)
+            zoomed_in_tilts.append(force_1)
+
+        zoomed_in_tilts = pd.concat(zoomed_in_tilts)
+        zoomed_in_tilts = zoomed_in_tilts.rename(columns={'index':'timeoffset'})
+
+        zoomed_in_tilts['timeoffset'] = linspace_1
+
         zoomed_in_df = zoomed_out_df[zoomed_out_df['event'] == 'CDistance']
         alert_row = zoomed_out_df[zoomed_out_df['event'] == 'Alert']
         zoomed_in_df = zoomed_in_df.append(alert_row)
         zoomed_in_df = zoomed_in_df.sort_index()
-    elif str(json_filename) == 'data/unlinked.json':
-        zoomed_in_df = []
 
-    return [zoomed_in_df, zoomed_out_df, zoomed_in_tilts, status]
+        return [zoomed_in_df, zoomed_in_tilts, zoomed_out_df, status]
+
+    elif str(json_filename) == 'data/unlinked.json':
+
+        zoomed_in_df = []
+        accel_df = df_categorised['detail']
+        detail_dict = accel_df[incident_number]
+        # Zoomed in data, use to get the speed in smaller interval
+        zoomed_in_df = pd.DataFrame.from_dict(detail_dict)
+
+        zoomed_in_df = zoomed_in_df.sort_values(by=['timeoffset'])
+
+        forces = zoomed_in_df['forces']
+        zoomed_in_tilts = []
+        for second in forces:
+            force_1 = pd.DataFrame.from_dict(second)
+            zoomed_in_tilts.append(force_1)
+
+        zoomed_in_tilts = pd.concat(zoomed_in_tilts)
+        zoomed_in_tilts = zoomed_in_tilts.rename(columns={'index':'timeoffset'})
+
+        zoomed_in_tilts['timeoffset'] = linspace_1
+
+        return [zoomed_in_df, zoomed_in_tilts]
 
 
 def get_data(json_filename):
@@ -61,6 +80,41 @@ def get_data(json_filename):
 
     return all_data
 
+
+def get_timeseries(tilts):
+    tilts = np.array(tilts)
+    tilts = tilts[:,1:]
+    tilt_timeseries = tilts.transpose()
+    tilt_timeseries = tilt_timeseries.astype('float')
+    tilt_timeseries = np.reshape(tilt_timeseries,(1,72,3))
+
+    return tilt_timeseries
+
+
+def get_tilt_timeseries(data):
+    # Extracts timeseries of tilts for whole dataset (output shape = (no_ts, len_ts, axes))
+
+    X = []
+    for i in tqdm(range(len(data))):
+        incident = data[i]
+        tilts = incident[1]
+        tilt_timeseries = get_timeseries(tilts)
+        X.append(tilt_timeseries)
+    X = np.array(X)
+    X = np.reshape(X, (X.shape[0], 72, 3))
+
+    return X
+
+def get_labels(cat_data):
+    y_test = []
+    for i in range(len(cat_data)):
+        incident = cat_data[i]
+        if 'Correct' in incident[3]:
+            y_test.append(1)
+        else:
+            y_test.append(0)
+
+    return y_test
 
 ###########################################################
 ### PICKLE FUNCTIONS                                    ###
