9098636a71588eb7877a9a59fa884197bfabd608
cwhite098 <gd19031@bristol.ac.uk>
2021-10-28 18:54:59 +0100

    Reworked feature_selection to be imported into nb

--- feature_selection.py
+++ feature_selection.py
@@ -12,13 +12,15 @@ import numpy as np
 from extract_data import *
 from sklearn.metrics import confusion_matrix
 
-def check_keyword(data2, keyword="Ignition-Off"):
+
+def check_keyword(incident, keyword="Ignition-Off"):
     # Returns the number of ignition-off events for an incident
     r = 0
-    data2 = data2["event"].values
+    data = incident[2]
+    data = data["event"].values
 
-    for i in range(len(data2)):
-        if data2[i] == keyword:
+    for i in range(len(data)):
+        if data[i] == keyword:
             r += 1
 
     return r #returns 0 if no keyword found
@@ -39,42 +41,59 @@ def get_max_vel_chng(incidentnum,data0):
             if abs(data0[i]-data0[i+1])>current:
                # print(abs(data0[i]-data0[i+1]))
                 current = abs(data0[i]-data0[i+1])
-    return current    
+    return current 
+
+def get_vel_change(incident):
+    data = incident[0]
+    data = data['speed'].values
+    d_v = data[6]-data[9]
+    return d_v
 
 
-def get_max_acc(incidentnum, data1):
-    current=0
+def get_max_acc(tilts):
+    accs = []
+    x = tilts[:,0]
+    y = tilts[:,1]
 
-    x = data1["tiltx"].values
-    y = data1["tilty"].values
-    z = data1["tiltz"].values
+    for i in range(len(x)):
+        # Get the acceleration in the horizontal plane
+        acc = np.linalg.norm([x[i], y[i]])
+        accs.append(acc)
 
-    for i in range(8):
-        if(x**2+y**2+z**2>current):
-            current = x**2+y**2+z**2
+    return np.max(accs)
 
-    return current
 
+def extract_features(data):
+    # Give data (cat/uncat) then recieve features array
+
+    ignition_event_list = []
+    stop_event_list = []
+    d_v_list = []
+    max_acc_list = []
+
+    tilts = get_tilt_timeseries(data)
+    tilts_no_z = calibrate_remove_z(tilts)
+
+    for incident in range(len(data)):
+        ig = check_keyword(data[incident], keyword='Ignition-Off')
+        st = check_keyword(data[incident], keyword='Stop')
+        d_v = get_vel_change(data[incident])
+        max_acc = get_max_acc(tilts_no_z[incident])
+
+        ignition_event_list.append(ig)
+        stop_event_list.append(st)
+        d_v_list.append(d_v)
+        max_acc_list.append(max_acc)
+
+
+    features = np.transpose(np.array([ignition_event_list, stop_event_list, d_v_list, max_acc_list]))
+
+    return features
 
 
 cat_data = load_list('pickle_data', 'cat_data')
 
-ignition_event_list = []
-for incident in cat_data:
-    r = check_keyword(incident[2], keyword='Ignition-Off')
-
-    ignition_event_list.append(r)
-
-print(ignition_event_list)
-y_pred = np.nonzero(ignition_event_list)
-
-y = get_labels(cat_data)
-print(y)
-
-print(confusion_matrix(y, ignition_event_list))
-
-
-    
+features = extract_features(cat_data)
     
     
     
