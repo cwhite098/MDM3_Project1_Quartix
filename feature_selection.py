33fcc059c22833b3e85e3ebfb59e4aa827f18000
patrickshortall26 <gz19139@bristol.ac.uk>
2021-11-09 14:47:58 +0000

    clean up keyword functions

--- feature_selection.py
+++ feature_selection.py
@@ -1,26 +1,44 @@
+from extract_data import *
+from sklearn.metrics import confusion_matrix
+from numpy.fft import rfft,rfftfreq,fft
 import numpy as np
 import pandas as pd
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-from extract_data import *
-from sklearn.metrics import confusion_matrix
-from numpy.fft import rfft,rfftfreq,fft
 import spectrum
 
+""" vvv KEYWORD FUNCTIONS vvv """
 
-#returns frequency of keyword
-def check_keyword(incident, keyword="Ignition-Off"):
-    # Returns the number of ignition-off events for an incident
-    r = 0
+def keyword_freq(incident, keyword):
+
+    freq = 0    # frequency of keyword
     data = incident[2]
     data = data["event"].values
 
     for i in range(len(data)):
         if data[i] == keyword:
-            r += 1
+            freq += 1
 
-    return r #returns 0 if no keyword found
+    return freq 
+
+def keyword_time_offset(incident, keyword):
+
+    data = incident[2]
+    time = 0    # time offset between keyword and alert (can only be positive i.e. keyword must be after alert)
+
+    event_series = data['event']
+    time_offset = data['timeoffset']
+    length_events = len(event_series)
+
+    for event in range(length_events):
+        if event_series[event] == keyword and time_offset[event] > 0:
+            time = time_offset[event]
+            break 
+
+    return time
+
+""" ^^^ KEYWORD FUNCTIONS ^^^ """
 
 def max_vel_0_time(incident):#maximum time velocity is 0
     speed = incident[0]["speed"].values
@@ -53,8 +71,7 @@ def mag_spike_difference(tilts): # a measure which captures whether or not there
     average = np.average(top_5[1:])
     difference = top_5[0]-average
 
-    return difference
-            
+    return difference            
             
 def get_max_vel_chng(incidentnum,data0):
     current=0
@@ -78,31 +95,6 @@ def get_vel_change(incident):
     data = data['speed'].values
     d_v = data[6]-data[9]
     return d_v
-
-
-
-#feature to see if ignition is turned off after the alert, and if so how long it took for that to happen 
-#returns 0 if ignition is not turned off, returns an interger value of time offset if ignition is turned off
-#if there are multiple ignition offs after the alert we take the 1st value
-#keywordtimechecker(incident)    
-#replace change kwarg for time offset of other keywords
-
-def keyword_time_checker(incident,keyword="Ignition-Off"):
-
-    data = incident[2]
-    ignition_time_off = 0
-
-    event_series = data['event']
-    time_offset = data['timeoffset']
-    length_events = len(event_series)
-
-    for event in range(length_events):
-        if event_series[event] == keyword and time_offset[event] > 0:
-            ignition_time_off = time_offset[event]
-            break 
-
-    return ignition_time_off
-
 
 def displacement_till_stop(incident):#returns distance from incident to ignition off
     data=incident[2]
@@ -128,9 +120,9 @@ def displacement_till_stop(incident):#returns distance from incident to ignition
 
 def get_std_xtilt(tilts):    
     return np.std(tilts[:,0])
+
 def get_std_ytilt(tilts):
     return np.std(tilts[:,1])
-
 
 def get_max_acc(tilts):
     accs = []
@@ -195,14 +187,15 @@ def periodogram_feauture_extractor(tilts_no_z,x_or_y):
 
 
 def extract_features(data):
-    # Give data (cat/uncat) then recieve features array
+    # Give data (cat/uncat), returns features array
 
-    ignition_event_list = []
-    stop_event_list = []
-    d_v_list = []
-    max_acc_list = []
+    ignition_freq_list = []
+    stop_freq_list = []
     ignition_times_list = []
     stop_time_list = []
+    
+    d_v_list = []
+    max_acc_list = []
     distance_list = []
     xstd = []
     ystd = []
@@ -230,24 +223,30 @@ def extract_features(data):
     tilts_no_z = calibrate_remove_z(tilts)
 
     for incident in range(len(data)):
-        ig = check_keyword(data[incident], keyword='Ignition-Off')
-        st = check_keyword(data[incident], keyword='Stop')
+        
+        ignition_freq = keyword_freq(data[incident], 'Ignition-Off')
+        stop_freq = keyword_freq(data[incident], 'Stop')
+        ignition_time = keyword_time_offset(data[incident], 'Ignition-Off')
+        stop_time = keyword_time_offset(data[incident], 'Stop')
+        
         d_v = get_vel_change(data[incident])
         max_acc = get_max_acc(tilts_no_z[incident])
-        ignition_time = keyword_time_checker(data[incident],keyword="Ignition-Off")
-        stop_time = keyword_time_checker(data[incident],keyword="Stop")
+        
         distance = distance_travelled(data[incident])
         x_power_1,x_power_2,x_power_3,x_power_4,x_frequency_1,x_frequency_2,x_frequency_3,x_frequency_4 = periodogram_feauture_extractor(tilts_no_z[incident],0)
         y_power_1,y_power_2,y_power_3,y_power_4,y_frequency_1,y_frequency_2,y_frequency_3,y_frequency_4 = periodogram_feauture_extractor(tilts_no_z[incident],1)
 
         difference = mag_spike_difference(tilts_no_z[incident])
         
-        ignition_event_list.append(ig)
-        stop_event_list.append(st)
-        d_v_list.append(d_v)
-        max_acc_list.append(max_acc)
+        
+        ignition_freq_list.append(ignition_freq)
+        stop_freq_list.append(stop_freq)
         ignition_times_list.append(ignition_time)
         stop_time_list.append(stop_time)
+
+        d_v_list.append(d_v)
+        max_acc_list.append(max_acc)
+        
         distance_list.append(distance)
         xstd.append(get_std_xtilt(tilts_no_z[incident]))
         ystd.append(get_std_ytilt(tilts_no_z[incident]))
@@ -271,7 +270,7 @@ def extract_features(data):
         y_power_4_list.append(y_power_4)
         
         
-    features = np.transpose(np.array([ignition_event_list, stop_event_list, d_v_list, max_acc_list, ignition_times_list, stop_time_list, 
+    features = np.transpose(np.array([ignition_freq_list, stop_freq_list, d_v_list, max_acc_list, ignition_times_list, stop_time_list, 
                                         distance_list, xstd, ystd, times_of_0_vel,x_power_1_list,x_power_2_list ,x_power_3_list, x_power_4_list, 
                                         mag_spike_difference_list, x_frequency_1_list, x_frequency_2_list, x_frequency_3_list, x_frequency_4_list,
                                         y_power_1_list, y_power_2_list, y_power_3_list, y_power_4_list, y_frequency_1_list, y_frequency_2_list, 
