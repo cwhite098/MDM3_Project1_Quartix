b102bd285cbb99657d2d58c27fa22b349cac5ec9
patrickshortall26 <gz19139@bristol.ac.uk>
2021-11-10 01:14:40 +0000

    final clean up

--- feature_selection.py
+++ feature_selection.py
@@ -9,7 +9,7 @@ import spectrum
 
 """ vvv KEYWORD FUNCTIONS vvv """
 
-def keyword_freq(incident, keyword): # returns the frequency count of a given keyword
+def keyword_freq(incident, keyword): # returns the frequency of a given keyword
 
     freq = 0    # frequency of keyword
     data = incident[2]
@@ -39,7 +39,7 @@ def keyword_time_offset(incident, keyword): # returns the time offset between th
 
 """ ^^^ KEYWORD FUNCTIONS ^^^ """
 
-""" vvv DISPLACEMENT, VELOCITY, ACCELERATION FUNCTIONS vvv """
+""" vvv DISTANCE AND SPEED FUNCTIONS vvv """
 
 def max_stop_time(incident): # returns the longest time the car is at rest for
 
@@ -60,7 +60,7 @@ def max_stop_time(incident): # returns the longest time the car is at rest for
                 
     return maximum
 
-def get_vel_change(incident): # returns the change in velocity between the alert and t = 3
+def get_vel_change(incident): # returns the change in speed between the alert and t = 3
     
     data = incident[0]
     data = data['speed'].values
@@ -68,7 +68,8 @@ def get_vel_change(incident): # returns the change in velocity between the alert
     
     return d_v
 
-def get_max_vel_chng(incident): # UNFINISHED
+def get_max_vel_chng(incident): # returns the largest change in speed in the zoomed in data
+    
     current = 0
     data0 = incident[0]
     data0 =data0["speed"].values
@@ -80,29 +81,23 @@ def get_max_vel_chng(incident): # UNFINISHED
                 current = abs(data0[i]-data0[i+1])
     return current 
 
-def displacement_till_stop(incident):#returns distance from incident to first ignition off
-    data=incident[2]
-    
+def distance_till_ig_off(incident): # returns distance from alert to first ignition off
+    data=incident[2]    
     
     time=keyword_time_offset(incident, 'Ignition-Off')
-    print(time)
-    #print(time)
     
-    #print(data.head)
     ids = data.index[data['timeoffset'] == time].tolist()[0]
     gridx=0
     gridy=0
-    #gridz=0
+    
     gridx=data.loc[data.index[ids], 'gridx']
     gridy=data.loc[data.index[ids], 'gridy']
-    #gridz=incident.loc[incident.index[ids], 'gridz']
-    #coordinates of stop
-    #displacement_till_stop(incident)
-    mag = gridx**2+gridy**2#+gridz**2
-    mag=mag**(1/2)
+
+    mag = gridx**2+gridy**2
+    mag = mag**(1/2)
     return mag
 
-def distance_travelled(incident):  # calculates the distance travelled after the alert 
+def distance_travelled(incident):  # returns the distance travelled after the alert 
     # grab zoomed out data
     data = incident[2]
 
@@ -118,14 +113,14 @@ def distance_travelled(incident):  # calculates the distance travelled after the
 
     return distance
 
-""" ^^^ DISPLACEMENT, VELOCITY, ACCELERATION FUNCTIONS ^^^ """
+""" ^^^ DISTANCE AND SPEED FUNCTIONS ^^^ """
 
-""" vvv TILTS FUNCTIONS vvv """
+""" vvv TILT FUNCTIONS (all use calibrated tilts) vvv """
 
-def mag_spike_difference(tilts): # a measure which captures whether or not there are a few spikes in acceleration or just one big one
+def mag_spike_difference(tilts_no_z): # returns the difference between the highest spike in acceleration and the average of the next 4 highest
     
     # magnitude at each time step
-    mags = np.linalg.norm(tilts, axis=1)
+    mags = np.linalg.norm(tilts_no_z, axis=1)
 
     # get top 5 mags
     sorted_mags = np.argsort(mags)
@@ -137,16 +132,16 @@ def mag_spike_difference(tilts): # a measure which captures whether or not there
 
     return difference            
 
-def get_std_xtilt(tilts):    
-    return np.std(tilts[:,0])
+def get_std_xtilt(tilts_no_z): # returns standard deviation of x tilts
+    return np.std(tilts_no_z[:,0])
 
-def get_std_ytilt(tilts):
-    return np.std(tilts[:,1])
+def get_std_ytilt(tilts_no_z): # returns standard deviation of y tilts
+    return np.std(tilts_no_z[:,1])
 
-def get_max_acc(tilts):
+def get_max_acc(tilts_no_z): # returns the max acceleration 
     accs = []
-    x = tilts[:,0]
-    y = tilts[:,1]
+    x = tilts_no_z[:,0]
+    y = tilts_no_z[:,1]
 
     for i in range(len(x)):
         # Get the acceleration in the horizontal plane
@@ -155,9 +150,7 @@ def get_max_acc(tilts):
 
     return np.max(accs)
 
-#function to return four largest powers and corresponding frequencies
-#usage: x_or_y = 0 for x values, 1 for y values
-def periodogram_feauture_extractor(tilts_no_z,x_or_y):
+def periodogram_feauture_extractor(tilts_no_z,x_or_y): # returns four largest powers and corresponding frequencies in x or y 
     number_data_points = 72
     s_s = 1/8 #sample spacing
     sum_tilts = tilts_no_z[:,x_or_y]
@@ -188,99 +181,117 @@ def periodogram_feauture_extractor(tilts_no_z,x_or_y):
 
     return power_1,power_2,power_3,power_4,frequency_1,frequency_2,frequency_3,frequency_4
 
-""" ^^^ TILTS FUNCTIONS ^^^ """
+""" ^^^ TILT FUNCTIONS (all use calibrated tilts) ^^^ """
 
-def extract_features(data):
-    # Give data (cat/uncat), returns features array
+def extract_features(data): # returns features
 
+    """ vvv initialise lists and get calibrated tilts vvv """
+
+    # keyword (4 features)
     ignition_freq_list = []
     stop_freq_list = []
     ignition_times_list = []
     stop_time_list = []
     
-    d_v_list = []
-    max_acc_list = []
-    distance_list = []
-    xstd = []
-    ystd = []
-    times_of_0_vel = []
+    # distance and speed (5 features)
+    max_stop_times_list = []
+    vel_change_list = []
+    max_vel_change_list = []
+    distance_till_ig_off_list = []
+    distance_travelled_list = []
+    
+    # tilts (4 features)
+    mag_spike_difference_list = []
+    x_std_dev_list = []
+    y_std_dev_list = []   
+    max_acc_list = []   
+    
+    # periodogram (16 features)
     x_power_1_list = []
     x_power_2_list= []
     x_power_3_list = []
     x_power_4_list = []
-    mag_spike_difference_list = []
+    
     x_frequency_1_list = []
     x_frequency_2_list = []
     x_frequency_3_list = []
     x_frequency_4_list = [] 
+    
     y_power_1_list = []
     y_power_2_list= []
     y_power_3_list = []
     y_power_4_list = []
+    
     y_frequency_1_list = []
     y_frequency_2_list = []
     y_frequency_3_list = []
     y_frequency_4_list = [] 
-    max_vel_changes = []
-
+    
+    # calibrate tilts
     tilts = get_tilt_timeseries(data)
     tilts_no_z = calibrate_remove_z(tilts)
 
+    """ ^^^ initialise lists and get calibrated tilts ^^^ """
+
+    """ vvv fill lists with data vvv """
+    
     for incident in range(len(data)):
         
-        ignition_freq = keyword_freq(data[incident], 'Ignition-Off')
-        stop_freq = keyword_freq(data[incident], 'Stop')
-        ignition_time = keyword_time_offset(data[incident], 'Ignition-Off')
-        stop_time = keyword_time_offset(data[incident], 'Stop')
-        
-        d_v = get_vel_change(data[incident])
-        max_acc = get_max_acc(tilts_no_z[incident])
-        
-        distance = distance_travelled(data[incident])
+        # extract periodogram data for current incident
         x_power_1,x_power_2,x_power_3,x_power_4,x_frequency_1,x_frequency_2,x_frequency_3,x_frequency_4 = periodogram_feauture_extractor(tilts_no_z[incident],0)
         y_power_1,y_power_2,y_power_3,y_power_4,y_frequency_1,y_frequency_2,y_frequency_3,y_frequency_4 = periodogram_feauture_extractor(tilts_no_z[incident],1)
-
-        difference = mag_spike_difference(tilts_no_z[incident])
         
+        # update keyword lists
+        ignition_freq_list.append(keyword_freq(data[incident], 'Ignition-Off'))
+        stop_freq_list.append(keyword_freq(data[incident], 'Stop'))
+        ignition_times_list.append(keyword_time_offset(data[incident], 'Ignition-Off'))
+        stop_time_list.append(keyword_time_offset(data[incident], 'Stop'))
         
-        ignition_freq_list.append(ignition_freq)
-        stop_freq_list.append(stop_freq)
-        ignition_times_list.append(ignition_time)
-        stop_time_list.append(stop_time)
+        # update distance and speed lists
+        max_stop_times_list.append(max_stop_time(data[incident]))
+        vel_change_list.append(get_vel_change(data[incident]))
+        max_vel_change_list.append(get_max_vel_chng(data[incident]))
+        distance_till_ig_off_list.append(distance_till_ig_off(data[incident]))
+        distance_travelled_list.append(distance_travelled(data[incident]))
         
-        max_vel_changes.append(get_max_vel_chng(data[incident]))
-        d_v_list.append(d_v)
-        max_acc_list.append(max_acc)
+        # update tilts lists
+        mag_spike_difference_list.append(mag_spike_difference(tilts_no_z[incident]))
+        x_std_dev_list.append(get_std_xtilt(tilts_no_z[incident]))
+        y_std_dev_list.append(get_std_ytilt(tilts_no_z[incident]))
+        max_acc_list.append(get_max_acc(tilts_no_z[incident]))
         
-        distance_list.append(distance)
-        xstd.append(get_std_xtilt(tilts_no_z[incident]))
-        ystd.append(get_std_ytilt(tilts_no_z[incident]))
-        times_of_0_vel.append(max_stop_time(data[incident]))
+        # update periodogram lists
         x_power_1_list.append(x_power_1)
         x_power_2_list.append(x_power_2)
         x_power_3_list.append(x_power_3)
         x_power_4_list.append(x_power_4)
-        mag_spike_difference_list.append(difference)
+        
         x_frequency_1_list.append(x_frequency_1)
         x_frequency_2_list.append(x_frequency_2)
         x_frequency_3_list.append(x_frequency_3)
         x_frequency_4_list.append(x_frequency_4)
-        y_frequency_1_list.append(y_frequency_1)
-        y_frequency_2_list.append(y_frequency_2)
-        y_frequency_3_list.append(y_frequency_3)
-        y_frequency_4_list.append(y_frequency_4)
+        
         y_power_1_list.append(y_power_1)
         y_power_2_list.append(y_power_2)
         y_power_3_list.append(y_power_3)
         y_power_4_list.append(y_power_4)
         
-        
-    features = np.transpose(np.array([ignition_freq_list, stop_freq_list, d_v_list, max_acc_list, ignition_times_list, stop_time_list, 
-                                        distance_list, xstd, ystd, times_of_0_vel,x_power_1_list,x_power_2_list ,x_power_3_list, x_power_4_list, 
-                                        mag_spike_difference_list, x_frequency_1_list, x_frequency_2_list, x_frequency_3_list, x_frequency_4_list,
-                                        y_power_1_list, y_power_2_list, y_power_3_list, y_power_4_list, y_frequency_1_list, y_frequency_2_list, 
-                                        y_frequency_3_list, y_frequency_4_list, max_vel_changes]))
+        y_frequency_1_list.append(y_frequency_1)
+        y_frequency_2_list.append(y_frequency_2)
+        y_frequency_3_list.append(y_frequency_3)
+        y_frequency_4_list.append(y_frequency_4)
 
+    """ ^^^ fill lists with data ^^^ """    
+
+    # add each list to features array    
+    features = np.transpose(np.array([ignition_freq_list, stop_freq_list, ignition_times_list, stop_time_list,
+                                      max_stop_times_list, vel_change_list, max_vel_change_list, distance_till_ig_off_list, distance_travelled_list,
+                                      mag_spike_difference_list, x_std_dev_list, y_std_dev_list, max_acc_list,
+                                      x_power_1_list, x_power_2_list, x_power_3_list, x_power_4_list,
+                                      x_frequency_1_list, x_frequency_2_list, x_frequency_3_list, x_frequency_4_list,
+                                      y_power_1_list, y_power_2_list, y_power_3_list, y_power_4_list,
+                                      y_frequency_1_list, y_frequency_2_list, y_frequency_3_list, y_frequency_4_list]))
+                                
     return features
 
 # load in the data
